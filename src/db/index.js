7cfddf41510ea2ca8bc55460cd8be6d4746b0a7c
Harsh Verma <hv0626266@gmail.com>
2025-07-26 23:49:31 +0530

    Remove unnecessary port log from MongoDB connection and add utility files for error handling and asynchronous operations

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,6 @@ const {DB_NAME} = require("../constants");
 const connectdb = async()=>{
     try {
         const connectioninstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        console.log(connectioninstance.connection.port);
         console.log(`mongodb successfully connected !! DB:HOST  ${connectioninstance.connection.host}`);
     } catch (error) {
         console.log("mongodb connection err:",error)
