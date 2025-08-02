3bf0206311c1ba8866a6fe7607a2e45ebf32b38b
Harsh Verma <hv0626266@gmail.com>
2025-08-02 19:20:15 +0530

    Add initial product controller and routes setup

---
+++ src/controllers/products.controllers.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const asynchandler = require("../utils/asynchandler");
+
+const apierror = require("../utils/apierror");
+
+const apiresponse = require("../utils/apierror");
+
+const products = require("../models/products.models");
+
+
+
+
+
+
+module.exports = {};
---
+++ src/routes/products.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {} = require("../controllers/products.controllers");
+
+const authorization = require("../middlewares/authorization.middlewares");
+
+
+
+
+
+module.exports = router;
