7e7629ebbe5c60d6ce07e098e0c091059106ad1c
Harsh Verma <hv0626266@gmail.com>
2025-08-21 20:25:28 +0530

    added validation for order items in publish order function to ensure required fields and valid values

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -9,10 +9,37 @@ const apiresponse = require("../utils/apiresponse");
 const Product = require("../models/products.models");
 
 const User = require("../models/users.models");
+const { all } = require('../app');
 
 
 const publishaorder = asynchandler(async(req,res)=>{
-    
+    const {orderitems,shippinginfo} = req.body;  
+
+    const allitemsinorderitems = ["product","quantity","priceatpackage"];
+
+    if(!Array.isArray(orderitems) || orderitems.length===0){
+        throw new apierror(400,"plz provide the orderitems");
+    }
+
+    orderitems.forEach((item,index)=>{
+        allitemsinorderitems.forEach((allitems)=>{
+            if(item[allitems]===undefined || item[allitems]===null){
+                throw new apierror(400,`Order item at index ${index} is missing required field: ${allitems}`);
+            }
+
+
+            if(typeof item.quantity!=="number" && item.quantity<=0){
+                throw new apierror(400,`Order item at index ${index} mujst have a value greater than zero`);
+            }
+
+            if (typeof item.priceatpackage !== "number" || item.priceatpackage < 0) {
+                throw new apierror(400, `Order item at index ${index} must have a valid price`);
+            }
+        })
+    });
+
+
+
 })
 
 
