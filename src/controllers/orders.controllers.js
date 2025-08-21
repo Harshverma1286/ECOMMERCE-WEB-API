52e4dbc58e2f26c2c83163dd2b608ca98402dd5d
Harsh Verma <hv0626266@gmail.com>
2025-08-21 19:27:59 +0530

    added orders controller and routes files

---
+++ src/controllers/orders.controllers.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const asynchandler = require("../utils/asynchandler");
+
+const apierror = require("../utils/apierror");
+
+const apiresponse = require("../utils/apiresponse");
+
+const Product = require("../models/products.models");
+
+const User = require("../models/users.models");
+
+
+
+
+
+
+
+module.exports = {};
---
+++ src/routes/orders.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {} = require("../controllers/orders.controllers");
+
+const authorization = require('../middlewares/authorization.middlewares');
+
+
+
+module.exports = router;
+
+
+
+
+
