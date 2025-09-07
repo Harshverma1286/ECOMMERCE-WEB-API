dc0c6407eff5110f3064d11c1d6a86ef9ea8ea2e
Harsh Verma <hv0626266@gmail.com>
2025-09-07 19:25:03 +0530

    feat: add functionality to remove products from the wishlist with validation

--- src/controllers/wishlist.controllers.js
+++ src/controllers/wishlist.controllers.js
@@ -93,7 +93,51 @@ const addaproductinthewishlist = asynchandler(async(req,res)=>{
     )
 })
 
+const removeaproductfromthewishlist = asynchandler(async(req,res)=>{
+    const {productId,wishlistId} = req.params;
+
+    if(!productId){
+        throw new apierror(400,"product id not found");
+    }
+
+    const product = await Product.findById(productId);
+
+    if(!product){
+        throw new apierror(404,"product does not exist");
+    }
+
+    if(!wishlistId){
+        throw new apierror(400,"wishlistId is required");
+    }
+
+    const wishlist = await Wishlist.findById(wishlistId);
+
+    if(!wishlist){
+        throw new apierror(404,"wishlist does not exist of the user");
+    }
+
+    if(wishlist.user.toString()!==req.user._id.toString()){
+        throw new apierror(403,"you are not permitted to remove the product from the wishlist");
+    }
+
+    const ifitis = wishlist.products.some((product)=> product._id.toString()===productId.toString());
+
+    if(!ifitis){
+        throw new apierror(404,"product does not exist in the wishlist");
+    }
+
+    wishlist.products = wishlist.products.filter((product)=> product._id.toString()!==productId.toString());
+
+    await wishlist.save();
+
+    return res.status(200).json(
+        new apiresponse(200,{},"product removed from the wishlist successfully")
+    )
+
+});
+
 
 module.exports = {createthewishlist,
     addaproductinthewishlist,
+    removeaproductfromthewishlist,
 };
--- src/routes/wishlist.routes.js
+++ src/routes/wishlist.routes.js
@@ -4,12 +4,18 @@ const router = express.Router();
 
 const authorization = require("../middlewares/authorization.middlewares");
 
-const { createthewishlist, addaproductinthewishlist } = require("../controllers/wishlist.controllers");
+const { createthewishlist, addaproductinthewishlist, removeaproductfromthewishlist } = require("../controllers/wishlist.controllers");
 
 
 
 router.route("/:productId/createawishlist").post(authorization,createthewishlist);
 
 router.route("/:wishlistId/:productId/addaproductinwishlist").post(authorization,addaproductinthewishlist);
+
+router.delete(
+    "/:wishlistId/products/:productId",
+    authorization,
+    removeaproductfromthewishlist
+);
 
 module.exports = router;
