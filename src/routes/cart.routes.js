ced8e00e1d40a9ccedaf9e7e9d98a073dfef5bad
Harsh Verma <hv0626266@gmail.com>
2025-09-09 17:47:40 +0530

    feat: initialize cart controller and router for cart functionality

---
+++ src/routes/cart.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+const authorization = require("../middlewares/authorization.middlewares");
+
+const {} = require("../controllers/cart.controllers");
+
+
+module.exports = router;
