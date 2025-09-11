5ca324d9703c5094c70f5e077e065d4036a614a9
Harsh Verma <hv0626266@gmail.com>
2025-09-11 18:58:55 +0530

    feat: add routes for adding items to cart and publishing a cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 const authorization = require("../middlewares/authorization.middlewares");
 
-const {} = require("../controllers/cart.controllers");
+const { addaproductinthecart, createacart } = require("../controllers/cart.controllers");
+
+router.route("/:cartID/additemsinthecart").post(authorization,addaproductinthecart);
+
+router.route("/publishacart").post(authorization,createacart);
 
 
 module.exports = router;
